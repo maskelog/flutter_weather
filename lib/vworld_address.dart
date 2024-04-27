3fc0cbf830c842ec3f830a1636a8d6c8ee2f4041
maskelog <blitzmaske@gmail.com>
2024-04-27 17:48:07 +0900

    vworld API 테스트

--- lib/vworld_address.dart
+++ lib/vworld_address.dart
@@ -1,8 +1,9 @@
+// vworld_address.dart
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:geolocator/geolocator.dart';
 import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:geolocator/geolocator.dart';
 
 class AddressFinder extends StatefulWidget {
   const AddressFinder({Key? key}) : super(key: key);
@@ -14,55 +15,63 @@ class AddressFinder extends StatefulWidget {
 class _AddressFinderState extends State<AddressFinder> {
   String _address = 'Press button to get your address';
 
-  Future<void> _getAddressFromCoordinates() async {
+  @override
+  void initState() {
+    super.initState();
+    getPositionAndAddress();
+  }
+
+  Future<void> getPositionAndAddress() async {
     try {
-      Position position = await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.high);
-      String apiKey = dotenv.env['VWORLDKEY']!;
-      Map<String, String> params = {
-        'key': apiKey,
-        'service': 'address',
-        'version': '2.0',
-        'request': 'getAddress',
-        'format': 'json',
-        'point': '${position.longitude},${position.latitude}',
-        'crs': 'epsg:4326',
-        'type': 'both',
-        'zipcode': 'true',
-        'simple': 'false'
-      };
+      Position? position = await Geolocator.getLastKnownPosition();
+      position ??= await Geolocator.getCurrentPosition();
+      getAddressFromCoordinates(position);
+    } catch (e) {
+      setState(() {
+        _address = 'Failed to get location: $e';
+      });
+    }
+  }
 
-      String baseUrl = "https://api.vworld.kr/req/address";
-      String queryString = Uri(queryParameters: params).query;
-      String requestUrl = '$baseUrl?$queryString';
+  Future<void> getAddressFromCoordinates(Position position) async {
+    String apiKey = dotenv.env['VWORLDKEY']!;
+    Map<String, String> params = {
+      'key': apiKey,
+      'service': 'address',
+      'version': '2.0',
+      'request': 'getAddress',
+      'format': 'json',
+      'point': '${position.longitude},${position.latitude}',
+      'crs': 'epsg:4326',
+      'type': 'both',
+      'zipcode': 'true',
+      'simple': 'false'
+    };
 
-      http.Response response = await http.get(Uri.parse(requestUrl));
+    String baseUrl = "https://api.vworld.kr/req/address";
+    String queryString = Uri(queryParameters: params).query;
+    String requestUrl = '$baseUrl?$queryString';
 
-      if (response.statusCode == 200) {
-        var jsonResponse = jsonDecode(response.body);
-        var responseResult = jsonResponse['response']['result'];
-        if (responseResult != null && responseResult.isNotEmpty) {
-          var addressText = responseResult[0]['text'];
-          setState(() {
-            _address = addressText ?? 'No address found';
-          });
-        } else {
-          print('Response result is empty or null: $jsonResponse');
-          setState(() {
-            _address = 'No address found';
-          });
-        }
-      } else {
-        print('Failed to fetch address. Status code: ${response.statusCode}');
+    http.Response response = await http.get(Uri.parse(requestUrl));
+
+    if (response.statusCode == 200) {
+      var jsonResponse = jsonDecode(response.body);
+      var responseResult = jsonResponse['response']['result'];
+      if (responseResult != null && responseResult.isNotEmpty) {
+        var addressText = responseResult[0]['text'];
         setState(() {
-          _address = 'Failed to fetch address';
+          _address = addressText ?? 'No address found';
+        });
+      } else {
+        print('Response result is empty or null: $jsonResponse');
+        setState(() {
+          _address = 'No address found';
         });
       }
-    } catch (e, stackTrace) {
-      print('Error occurred: $e');
-      print('Stack trace: $stackTrace');
+    } else {
+      print('Failed to fetch address. Status code: ${response.statusCode}');
       setState(() {
-        _address = 'Error occurred: $e';
+        _address = 'Failed to fetch address';
       });
     }
   }
@@ -73,13 +82,13 @@ class _AddressFinderState extends State<AddressFinder> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          ElevatedButton(
-            onPressed: _getAddressFromCoordinates,
-            child: const Text('Find My Address'),
-          ),
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Text(_address),
+            child: Text(_address, textAlign: TextAlign.center),
+          ),
+          ElevatedButton(
+            onPressed: getPositionAndAddress,
+            child: const Text('Get Address'),
           ),
         ],
       ),
